b0dc0a083dcbf67847e0aeaf67cf91aa0b51bb10
nicodellolio <151004233+nicodellolio@users.noreply.github.com>
2024-05-21 15:07:11 +0200

    small fixes to the show route

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -11,23 +11,24 @@
                         <th scope="col">Description</th>
                         <th scope="col">Project Start</th>
                         <th scope="col">Project End</th>
-                        <th scope="col">Link to Source Code</th>
-                        <th scope="col">Link to Project View</th>
+                        <th scope="col">Show</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <a class="text-decoration-none" href="{{ route('admin.projects.show', $project) }}">cane
-                    </a>
                     @foreach ($projects as $project)
                         <tr class="">
-                                <td scope="row">{{ $project->title }}</td>
-                                <td scope="row">{{ $project->description }}</td>
-                                <td scope="row">{{ $project->project_start_date }}</td>
-                                <td scope="row">{{ $project->project_end_date }}</td>
-                                <td scope="row">{{ $project->link_to_source_code }}</td>
-                                <td scope="row">{{ $project->link_to_project_view }}</td>
+                            <td scope="row">{{ $project->title }}</td>
+                            <td scope="row">{{ $project->description }}</td>
+                            <td scope="row">{{ $project->project_start_date }}</td>
+                            <td scope="row">{{ $project->project_end_date }}</td>
+                            <td>
+                                <a class="text-decoration-none text-dark fs-4" href="{{ route('admin.projects.show', $project) }}">
+                                    <i class="fa fa-eye" aria-hidden="true"></i>
+                                </a>
+                            </td>
                         </tr>
                     @endforeach
+
                 </tbody>
             </table>
         </div>
